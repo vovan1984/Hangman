a3f1d2f1e128f0110339c1bf552b3db13175dd8b
Vladimir Igumnov <vladimir.igumnov@gmail.com>
2019-01-30 22:25:28 -0500

    Adding JUnit5 support in pom.xml

---
+++ src/test/java/hangman/HangmanDictionaryTest.java
@@ -0,0 +1,30 @@
+package hangman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class HangmanDictionaryTest
+{
+    /**
+     * Validate creation of default dictionary 
+     * from resources included to the project.
+     */
+    @Test
+    @Disabled
+    void testDefaultDictionaryCreation()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Validate creation of dictionary from a file defined
+     * by a user.
+     */
+    @Test
+    void testDictionaryCreationFromFile()
+    {
+        fail("Not yet implemented");
+    }
+}
