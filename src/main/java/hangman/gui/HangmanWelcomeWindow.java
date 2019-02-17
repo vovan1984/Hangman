49769a50afd4b2816f5b4c825565585295ef60ac
Vladimir Igumnov <vladimir.igumnov@gmail.com>
2019-02-17 13:18:55 -0500

    Enhancing readability of HangmanWelcomeWindow.java by extracting private methods

--- src/main/java/hangman/gui/HangmanWelcomeWindow.java
+++ src/main/java/hangman/gui/HangmanWelcomeWindow.java
@@ -1,18 +1,14 @@
 package hangman.gui;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Label;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
 import hangman.HangmanDictionary;
 import hangman.HangmanStats;
 
@@ -35,7 +31,7 @@ public class HangmanWelcomeWindow extends HangmanWindow implements ActionListene
     // set default font for a name of the game
     private static final Font DEF_TITLE_FONT = new Font("Serif", Font.PLAIN, 80);
     
-    private Label nameLabel; 
+    private JLabel nameLabel; 
     private String title;
     private HangmanDictionary dictionary;
     private HangmanStats playersInfo;
@@ -50,41 +46,24 @@ public class HangmanWelcomeWindow extends HangmanWindow implements ActionListene
         this.dictionary = dictionary;
         this.playersInfo = playersInfo;
         
-        // add button
-        JButton newGameButton = new JButton("NEWGAME");
-        newGameButton.setOpaque(true);
-        newGameButton.setFocusPainted(false);
-        newGameButton.setBorderPainted(false);
-        newGameButton.setFont(DEF_FONT);
-        newGameButton.setBackground(LIGHT_BLUE); // light blue button
-        newGameButton.setForeground(Color.WHITE);
-        newGameButton.setPreferredSize(new Dimension(DEF_BUTTON_WIDTH, DEF_BUTTON_HEIGHT));
+        // create button for starting new game.
+        JButton newGameButton = createNewGameButton();
         
-        setFocusable(true); // has to be focusable to receive key events
-        getRootPane().setDefaultButton(newGameButton);
-
         // place button to the center of a lower panel
         lowerPane.setLayout(new GridBagLayout());
         lowAlignment = new GridBagConstraints();
         lowAlignment.anchor = GridBagConstraints.CENTER;      
-        
-        newGameButton.addActionListener(this); // handler for button pressed event
-
         lowerPane.add(newGameButton, lowAlignment);
-        
-        upperPane.setLayout(new GridBagLayout());
-        upAlignment = new GridBagConstraints();
-        upAlignment.fill = GridBagConstraints.BOTH;       
-        upAlignment.weightx = 0.4;
-        upAlignment.weighty = 0.6; // cell takes 75% of the area height
-        upAlignment.gridwidth = 1;
-        upAlignment.gridx = 1; // coordinates of the cell
-        upAlignment.gridy = 1;     
-        
-        // add label with the name of the game
-        nameLabel = new Label(GAME_NAME, Label.CENTER);
+                
+        // create label with the name of the game
+        nameLabel = new JLabel(GAME_NAME, JLabel.CENTER);
         nameLabel.setFont(DEF_TITLE_FONT);
         nameLabel.setForeground(Color.WHITE);
+        
+        // add label to the center of an upper panel
+        upperPane.setLayout(new GridBagLayout());
+        upAlignment = new GridBagConstraints();
+        upAlignment.fill = GridBagConstraints.BOTH;  
         upperPane.add(nameLabel, upAlignment);
 
         // handler for resize event
@@ -99,6 +78,32 @@ public class HangmanWelcomeWindow extends HangmanWindow implements ActionListene
         
         // display the window
         setVisible(true);
+    }
+
+    /**
+     * This method is used to create a button for starting new game.
+     * It is setup to accept clicks and pressing of Enter.
+     * 
+     * @return New Game button.
+     */
+    private JButton createNewGameButton()
+    {
+        // add button
+        JButton newGameButton = new JButton("NEWGAME");
+        newGameButton.setOpaque(true);
+        newGameButton.setFocusPainted(false);
+        newGameButton.setBorderPainted(false);
+        newGameButton.setFont(DEF_FONT);
+        newGameButton.setBackground(LIGHT_BLUE); // light blue button
+        newGameButton.setForeground(Color.WHITE);
+        newGameButton.setPreferredSize(new Dimension(DEF_BUTTON_WIDTH, DEF_BUTTON_HEIGHT));
+        
+        // Clicking on button or pressing Enter should open LeaderBoard window.
+        setFocusable(true); // has to be focusable to receive key events
+        getRootPane().setDefaultButton(newGameButton);
+        newGameButton.addActionListener(this); // handler for button clicked event
+        
+        return newGameButton;
     }
     
     @Override
